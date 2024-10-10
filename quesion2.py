549495c69ce7ace1fc92abe23f72b6c3557acfef
phionah mugisha <phoniahtumusiime99@gmail.com>
2024-10-10 11:05:23 +0300

    test1

---
+++ quesion2.py
@@ -0,0 +1,8 @@
+# create a simple programe that calculate the volume of a cylinder 
+radius=int(Enter the radius)
+height=int(enter the height)
+pie=3\4
+volume=3.2
+r*2
+r=2*2
+r=4
---
+++ test2.py
@@ -0,0 +1,8 @@
+# Question 1 a
+# calculate the area of the triangle 
+# write a programe that calcualate 
+base=int(input("Enter the area of the triangle"))
+height=int(input("Enter the height,"))
+area=1\2*(base*height)
+base=4
+height=8
